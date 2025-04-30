1586f588dc1e6364ce1d92668fefad092a7112b3
Umar Jan <Umarabidjan@gmail.com>
2025-04-30 00:00:29 -0400

    Add new UI pages
    Add Help and Settings pages, update Standings component, and modify .gitignore

--- Client/F1Dash/src/main.tsx
+++ Client/F1Dash/src/main.tsx
@@ -1,33 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Schedule from './Pages/Schedule.tsx'
-import Dashboard from './Pages/Dashboard.tsx'
-import Standings from './Pages/Standings.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Schedule from "./Pages/Schedule.tsx";
+import Dashboard from "./Pages/Dashboard.tsx";
+import Standings from "./Pages/Standings.tsx";
+import Help from "./Pages/Help.tsx";
+import Settings from "./Pages/Settings.tsx";
 
 const router = createBrowserRouter([
   {
-  path: '/',
-  element: <App />,
-  }, 
+    path: "/",
+    element: <App />,
+  },
   {
-    path: '/schedule',
+    path: "/schedule",
     element: <Schedule />,
   },
   {
-    path: '/dashboard',
-    element: <Dashboard/>
+    path: "/dashboard",
+    element: <Dashboard />,
   },
   {
-    path: '/standings',
-    element: <Standings/>
-  }
+    path: "/standings",
+    element: <Standings />,
+  },
+  {
+    path: "/settings",
+    element: <Settings />,
+  },
+  {
+    path: "/help",
+    element: <Help />,
+  },
 ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
